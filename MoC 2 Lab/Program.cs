a50542b986ddc9d64ae14f2cf53e1a726822bbb8
pride234 <shinigami466914@gmail.com>
2020-11-01 00:40:35 +0200

    first commit, lab2 is created

---
+++ MoC 2 Lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoC_2_Lab {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
