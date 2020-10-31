54d44eb00f3034452e46ad6bad888cd592be5994
pride234 <shinigami466914@gmail.com>
2020-11-01 01:11:56 +0200

    added text reading and text formatting

--- MoC 2 Lab/Program.cs
+++ MoC 2 Lab/Program.cs
@@ -2,11 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace MoC_2_Lab {
     class Program {
         static void Main(string[] args) {
+
+            StreamReader origin = new StreamReader(@"C:\Users\PRIDE\source\repos\MoC 2 Lab\text.txt");
+
+            string text = origin.ReadToEnd();
+            //string curLine = "";
+            //string text = "";
+            //while ((curLine = cp.ReadLine()) != null) text += curLine;
+            origin.Close();
+
+            text = text.Replace("ґ", "г");
+            text = text.ToLower();
+
+            Regex rgx = new Regex("[^а-яі]");
+            text = rgx.Replace(text, "");
+            //Console.WriteLine(text);
+
+            StreamWriter form = new StreamWriter(@"C:\Users\PRIDE\source\repos\MoC 2 Lab\text1.txt");
+
+            form.Write(text);
+            form.Close();
         }
     }
 }
